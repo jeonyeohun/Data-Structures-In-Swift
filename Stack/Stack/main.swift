936205c00e5a53199d8af463fce31dc8388e7963
Jeon, Yeo Hun <hunihun956@gmail.com>
2021-08-01 20:06:34 +0900

    :sparkles: Add Stack

---
+++ Stack/Stack/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  Stack
+//
+//  Created by 전여훈 on 2021/08/01.
+//
+
+import Foundation
+
+
+import Foundation
+
+struct Stack<T> {
+    var elements : [T] = []
+    
+    init() {
+   
+    }
+    
+    var count : Int {
+        return elements.count
+    }
+    
+    var isEmpty : Bool {
+        return elements.isEmpty
+    }
+    
+    mutating func pop () -> T? {
+        return elements.popLast()
+    }
+    
+    mutating func push (_ element: T) {
+        elements.append(element)
+    }
+    
+    func top() -> T? {
+        return elements.last
+    }
+    
+}
