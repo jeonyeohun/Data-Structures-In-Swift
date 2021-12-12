57ab9925bed79adbe367bf7fc5114d70e3f14ab3
jeonyeohun <21500630@handong.edu>
2021-12-12 15:04:03 +0900

    :sparkles: Add generic, build heap, test cases

--- Heap/Heap/Heap.swift
+++ Heap/Heap/Heap.swift
@@ -11,9 +11,19 @@ struct Heap<T: Comparable> {
     private var elements: [T]
     private let sortFunction: (T, T) -> Bool
     
-    init(elements: [T], sortFunction: @escaping (T, T) -> Bool) {
+    var isEmpty: Bool {
+        return elements.isEmpty
+    }
+    var peak: T? {
+        return self.elements.first
+    }
+    
+    init(elements: [T] = [], sortFunction: @escaping (T, T) -> Bool) {
         self.elements = elements
         self.sortFunction = sortFunction
+        if !elements.isEmpty {
+            self.buildHeap()
+        }
     }
     
     func leftChild(of index: Int) -> Int {
@@ -29,26 +39,31 @@ struct Heap<T: Comparable> {
         elements.append(element)
     }
     mutating func diveDown(from index: Int) {
-        var largest = index
+        var higherPriority = index
         let leftIndex = self.leftChild(of: index)
         let rightIndex = self.rightChild(of: index)
         
-        if leftIndex < elements.endIndex && self.sortFunction(elements[leftIndex], elements[largest]) {
-            largest = leftIndex
+        if leftIndex < elements.endIndex && self.sortFunction(elements[leftIndex], elements[higherPriority]) {
+            higherPriority = leftIndex
         }
-        if rightIndex < elements.endIndex && self.sortFunction(elements[rightIndex], elements[largest]) {
-            largest = rightIndex
+        if rightIndex < elements.endIndex && self.sortFunction(elements[rightIndex], elements[higherPriority]) {
+            higherPriority = rightIndex
         }
-        if largest != index {
-            elements.swapAt(largest, index)
-            self.diveDown(from: largest)
+        if higherPriority != index {
+            elements.swapAt(higherPriority, index)
+            self.diveDown(from: higherPriority)
         }
     }
     mutating func swimUp(from index: Int) {
         var index = index
-        while index > 1 && self.elements[index/2] < self.elements[index] {
+        while index >= 0 && self.sortFunction(self.elements[index], self.elements[index/2]) {
             self.elements.swapAt(index/2, index)
             index /= 2
+        }
+    }
+    mutating func buildHeap() {
+        for index in (0...(self.elements.count / 2)).reversed() {
+            self.diveDown(from: index)
         }
     }
     mutating func insert(node: T) {
@@ -56,10 +71,10 @@ struct Heap<T: Comparable> {
         self.swimUp(from: self.elements.endIndex - 1)
     }
     mutating func remove() -> T? {
-        if self.elements.count == 1 { return nil }
-        self.elements.swapAt(1, elements.endIndex - 1)
+        if self.elements.isEmpty { return nil }
+        self.elements.swapAt(0, elements.endIndex - 1)
         let deleted = elements.removeLast()
-        self.diveDown(from: 1)
+        self.diveDown(from: 0)
         
         return deleted
     }
--- Heap/Heap/main.swift
+++ Heap/Heap/main.swift
@@ -5,3 +5,52 @@
 //
 
 import Foundation
+
+print("\n======Test Min Heap ======")
+var heap = Heap<Int>(sortFunction: <)
+
+for number in 0..<10 {
+    heap.insert(node: number)
+}
+while !heap.isEmpty {
+    print(heap.remove())
+}
+
+//Optional(0)
+//Optional(1)
+//Optional(2)
+//Optional(3)
+//Optional(4)
+//Optional(5)
+//Optional(6)
+//Optional(7)
+//Optional(8)
+//Optional(9)
+
+print("\n======Test Max Heap ======")
+heap = Heap<Int>(sortFunction: >)
+
+for number in 0..<10 {
+    heap.insert(node: number)
+}
+while !heap.isEmpty {
+    print(heap.remove())
+}
+
+//Optional(9)
+//Optional(8)
+//Optional(7)
+//Optional(6)
+//Optional(5)
+//Optional(4)
+//Optional(3)
+//Optional(2)
+//Optional(1)
+//Optional(0)
+
+print("\n======Test Build Heap ======")
+heap = Heap<Int>(elements: [9, 8, 7, 6, 5, 4, 3, 2, 1, 0], sortFunction: <)
+
+while !heap.isEmpty {
+    print(heap.remove())
+}
